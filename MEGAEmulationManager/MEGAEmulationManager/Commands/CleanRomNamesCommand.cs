96f8dcddcfe096b50b990d59237798b857aeffeb
Tony <tonygithub@outlook.com>
2015-03-05 21:16:41 -0800

    Add command unbound to source control

---
+++ MEGAEmulationManager/MEGAEmulationManager/Commands/CleanRomNamesCommand.cs
@@ -0,0 +1,19 @@
+﻿using MEGAEmulationManager.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MEGAEmulationManager.Commands
+{
+    public class CleanRomNamesCommand : ICommand
+    {
+        private EmuManagerViewModel _viewmodel;
+        public CleanRomNamesCommand(EmuManagerViewModel viewmodel)
+        {
+            _viewmodel = viewmodel;
+        }
+    }
+}
