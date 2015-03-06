1731da990e3f0264f0862c99cf90940c352a55d0
Tony <tonygithub@outlook.com>
2015-03-05 18:04:43 -0800

    UI and architecture

---
+++ MEGAEmulationManager/MEGAEmulationManager/ViewModels/EmuManagerViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MEGAEmulationManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEGAEmulationManager.ViewModels
+{
+    public class EmuManagerViewModel
+    {
+        public EmuManagerModel _model;
+
+        public EmuManagerViewModel()
+        {
+            _model = new EmuManagerModel();
+        }
+
+        
+    }
+}
