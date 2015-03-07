5115e51f225a5be002de2a952212bb87543649c3
Tony <tonygithub@outlook.com>
2015-03-06 16:40:40 -0800

    Refactor

--- MEGAEmulationManager/MEGAEmulationManager/ViewModels/EmuManagerViewModel.cs
+++ MEGAEmulationManager/MEGAEmulationManager/ViewModels/EmuManagerViewModel.cs
@@ -15,52 +15,58 @@ namespace MEGAEmulationManager.ViewModels
 
         #region ICommands and Related Properties
         public ICommand BrowseRomDirectoryCommand { get; private set; }
-        public bool CanBrowseRomDirectory { get { return true; } }
 
         public ICommand BrowseEmulatorDirectoryCommand { get; private set; }
-        public bool CanBrowseEmulatorDirectory { get { return true; }}
         
         public ICommand LoadRomsIntoGURUCommand { get; private set; }
-        public bool CanLoadRomsIntoGURU { get { return true; } }
         
         public ICommand CleanRomNamesCommand { get; private set; }
-        public bool CanCleanRomNames { get { return true; } }
         
         public ICommand CheckRomStreamingCompatibilityCommand { get; private set; }
-        public bool CanCheckRomStreamingCompatibility { get { return true; } }
         
         public ICommand FixRomStreamingCompatibilityCommand { get; private set; }
-        public bool CanFixRomStreamingCompatibility { get { return true; } }
         
         public ICommand CreateSteamShortcutsCommand { get; private set; }
-        public bool CanCreateSteamShortcuts { get { return true; } }
         
         public ICommand DeleteSteamShortcutsCommand { get; private set; }
-        public bool CanDeleteSteamShortcuts { get { return true; } }
+
+        //TODO: All of these need actual checks.
+        public bool CanLoadRomsIntoGURU 
+        { 
+            get { return true; } 
+        }
+        public bool CanCleanRomNames 
+        { 
+            get { return true; } 
+        }
+        public bool CanCheckRomStreamingCompatibility 
+        { 
+            get { return true; } 
+        }
+        public bool CanFixRomStreamingCompatibility 
+        { 
+            get { return true; } 
+        }
+        public bool CanCreateSteamShortcuts 
+        { 
+            get { return true; } 
+        }
+        public bool CanDeleteSteamShortcuts 
+        { 
+            get { return true; } 
+        }
         #endregion
 
         public EmuManagerViewModel()
         {
             EmuManagerModel = new EmuManagerModel();
 
-            //ICommand BrowseRomDirectoryCommand = new Command(this);
-            //ICommand BrowseEmulatorDirectoryCommand = new Command(this);
             ICommand LoadRomsIntoGURUCommand = new LoadRomsIntoGURUCommand(this);
             ICommand CleanRomNamesCommand = new CleanRomNamesCommand(this);
             ICommand CheckRomStreamingCompatibilityCommand = new CheckRomStreamingCompatibilityCommand(this);
             ICommand FixRomStreamingCompatibilityCommand = new FixRomStreamingCompatibilityCommand(this);
             ICommand CreateSteamShortcutsCommand = new CreateSteamShortcutsCommand(this);
             ICommand DeleteSteamShortcutsCommand = new DeleteSteamShortcutsCommand(this);
-        }
-
-        public void BrowseRomDirectory()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void BrowseEmulatorDirectory()
-        {
-            throw new System.NotImplementedException();
         }
 
         public void LoadRomsIntoGURU()
