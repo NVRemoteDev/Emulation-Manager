7cca518643198e102fb03b572cfec167dc5f6084
Tony <tonygithub@outlook.com>
2015-03-07 07:16:10 -0800

    Initial commit

---
+++ MEGAEmulationManager/MEGAEmulationManager/Helpers/IOHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEGAEmulationManager.Helpers
+{
+    public static class IOHelper
+    {
+        public async static Task<int> EnumerateDirectories(string)
+        {
+
+        }
+    }
+}
