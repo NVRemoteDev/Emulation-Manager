e0770e1be2698373c30234f240602597eb882daa
Tim Miller <innerlogic4321@gmail.com>
2015-04-19 14:17:20 -0400

    Add debug manager, show error message function

---
+++ EmulationManager/EmulationManager/Helpers/DebugManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EmulationManager.Helpers
+{
+    public static class DebugManager
+    {
+        public static MessageBoxResult ShowErrorDialog(string errorMessage, Exception exception)
+        {
+            return MessageBox.Show(errorMessage + Environment.NewLine + exception.Message, "ERROR", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+        }
+    }
+}
