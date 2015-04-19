3ba8b2a4cc9e5c846868bd5dc1d44847766b06b1
Tim Miller <t_miller@outlook.com>
2015-04-19 15:27:02 -0400

    Add NotifierBase.cs

---
+++ EmulationManager/EmulationManager/Common/NotifierBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using EmulationManager.Properties;
+
+namespace EmulationManager.Common
+{
+    public abstract class NotifierBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void RaiseEvent<TArgs>(EventHandler<TArgs> handler, TArgs args)
+        {
+            if(handler!=null)
+                handler.Invoke(this, args);
+        }
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] String propertyName = null)
+        {
+            if (Equals(storage, value)) return false;
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
